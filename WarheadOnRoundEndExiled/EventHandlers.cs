f6aa80d56f053472e46d7f5a115214cdd2ee53da
Sora Cent <christofgmur@gmail.com>
2020-12-25 19:34:37 +0100

    Projektdateien hinzufügen.

---
+++ WarheadOnRoundEndExiled/EventHandlers.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEC;
+
+namespace WarheadOnRoundEndExiled
+{
+    public class EventHandler
+    {
+        public WarheadOnRoundEndExiled plugin;
+        public EventHandler(WarheadOnRoundEndExiled plugin) => this.plugin = plugin;
+
+        public void OnEndingRoundEvent(EndingRoundEventArgs ev)
+        {
+            if (ev.IsAllowed && ev.IsRoundEnded && !Warhead.IsDetonated)
+            {
+                Warhead.Start();
+                Warhead.DetonationTimer = 5;
+                Warhead.Shake();
+            }
+        }
+    }
+}
---
+++ WarheadOnRoundEndExiled/Plugins.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Features;
+
+namespace WarheadOnRoundEndExiled
+{
+    public class WarheadOnRoundEndExiled : Plugin<Config>
+    {
+        public static WarheadOnRoundEndExiled WarheadOnRoundEndExiledRef { get; private set; }
+        public override string Name => nameof(WarheadOnRoundEndExiled);
+        public override string Author => "SoraCént";
+        public EventHandler Handler;
+
+        public WarheadOnRoundEndExiled()
+        {
+            WarheadOnRoundEndExiledRef = this;
+        }
+
+        public override void OnEnabled()
+        {
+            Log.Info("WarheadOnRoundEndExiled Plugin Aktiviert!");
+            Handler = new EventHandler(this);
+            Exiled.Events.Handlers.Server.EndingRound += Handler.OnEndingRoundEvent;
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.EndingRound -= Handler.OnEndingRoundEvent;
+            Handler = null;
+        }
+
+        public override void OnReloaded() { }
+    }
+}
